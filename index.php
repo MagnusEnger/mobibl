19b70298a17a14613f4130bb04a2afea29cdb685
Magnus Enger <magnus@enger.priv.no>
2011-07-28 13:34:54 +0200

    Make sure moBibl is the page name

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 	<head> 
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
 	<meta charset="UTF-8">
-	<title>Page Title</title>
+	<title>moBibl</title>
 	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.css" />
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.6.1.min.js"></script>
 	<script type="text/javascript" src="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.js"></script>
 </head> 
 <body> 
 
-<div data-role="page" data-title="Forsiden" id="index" data-theme="b">
+<div data-role="page" data-title="moBibl" id="index" data-theme="b">
 
 	<div data-role="header">
 		<h1>moBibl <sup>beta</sup></h1>
