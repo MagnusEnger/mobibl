a3ab62a19e86b9aa7058a0c44d3da547cd94a9ee
Magnus Enger <magnus@enger.priv.no>
2011-08-15 21:50:38 +0200

    Switch to using jQuery Mobile Beta 2

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
   <meta charset="UTF-8">
   <title>moBibl</title>
-  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.css" />
+  <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b2/jquery.mobile-1.0b2.min.css" />
   <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.1.min.js"></script>
   <script type="text/javascript" src="mobibl.js"></script>
   <script type="text/javascript" src="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.js"></script>
