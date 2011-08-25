a6c41fab842386ed368476995c31382a9a94f9a9
Magnus Enger <magnus@enger.priv.no>
2011-08-25 08:27:38 +0200

    Forgot to change the JS to beta 2...

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <link rel="stylesheet" href="http://code.jquery.com/mobile/1.0b2/jquery.mobile-1.0b2.min.css" />
   <script type="text/javascript" src="http://code.jquery.com/jquery-1.6.1.min.js"></script>
   <script type="text/javascript" src="mobibl.js"></script>
-  <script type="text/javascript" src="http://code.jquery.com/mobile/1.0b1/jquery.mobile-1.0b1.min.js"></script>
+  <script type="text/javascript" src="http://code.jquery.com/mobile/1.0b2/jquery.mobile-1.0b2.min.js">
 </head> 
 <body> 
 
